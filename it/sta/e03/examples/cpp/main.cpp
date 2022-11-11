5dd06026b0b52a9fbecfc35e72aac333551d67a3
Jakub Januszewski <79792011+Janirr@users.noreply.github.com>
2022-11-11 12:16:50 +0100

    Multiple authors by vector

--- it/sta/e03/examples/cpp/main.cpp
+++ it/sta/e03/examples/cpp/main.cpp
@@ -7,11 +7,14 @@
 
 int main() {
     ISBN isbn("0735619654");
-    vector <Author> author;
-    Author author("David", "West");
+    std::vector <Author> authors;
+    Author auth1("David", "West");
+    authors.push_back(auth1);
+    Author auth2("Artur", "East");
+    authors.push_back(auth2);
     Title title("Myślenie obiektowe", "Object Thinking");
     int pages = 368;
-    Book objectThinking(isbn, author, title, pages);
+    Book objectThinking(isbn, authors, title, pages);
     std::cout << objectThinking.toJSON();
     return 0;
 }
