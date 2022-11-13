6f2c98b72b5501ae55ccb563e26adcbdd9e14911
Jakub Hamerliński <jakub.hamerlinski@graphpad.com>
2022-11-14 00:21:18 +0100

    refactor!: change repo structure second time

--- code-examples/java/e04/src/lesson4/Cash.java
+++ code-examples/java/e04/src/lesson4/Cash.java
@@ -1,21 +1,21 @@
 package lesson4;
 
 interface Money {
-  Money multipliedMoney(float factor);
+  Money multiplied(float factor);
   String balance();
 }
 
 public class Cash implements Money {
   private final float dollars;
   @Override
-  public Cash multipliedMoney(float factor) { return new Cash(this.dollars * factor); }
+  public Cash multiplied(float factor) { return new Cash(this.dollars * factor); }
   @Override
   public String balance() { return "$" + dollars; }
   public Cash(float dollars) { this.dollars = dollars; }
 
   public static void main(String[] args) {
     Cash smallAmount = new Cash(15.0f);
-    Cash bigAmount = smallAmount.multipliedMoney(10f);
+    Cash bigAmount = smallAmount.multiplied(10f);
     System.out.println("I have " + bigAmount.balance());
   }
 }
