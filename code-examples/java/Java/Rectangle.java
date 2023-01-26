1c8942b54da5b5bf2a75051f54ec23a5e526e510
MNOWAK1234 <108874597+MNOWAK1234@users.noreply.github.com>
2023-01-26 17:24:43 +0100

    Example of subtyping
    Lesson number8

---
+++ code-examples/java/Java/Rectangle.java
@@ -0,0 +1,16 @@
+package Java;
+
+public class Rectangle implements Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+}
---
+++ code-examples/java/Java/Shape.java
@@ -0,0 +1,5 @@
+package Java;
+
+public interface Shape {
+    public double getArea();
+}
