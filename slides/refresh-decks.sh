5459a3cbb07bf4b0683a69b115358287e77c507b
Jakub Piotr Hamerliński <jakub.hamerlinski.put@gmail.com>
2023-03-16 15:39:44 +0100

    fix: adjust copyright memo

--- slides/refresh-decks.sh
+++ slides/refresh-decks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2023 Jakub Piotr Hamerliński
+# Copyright © 2023 Jakub Piotr Hamerliński
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
