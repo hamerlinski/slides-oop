fa35f0bfd297c9964d3fbe9b3ee9149d7b43490f
Jakub Hamerliński <jakub.hamerlinski@dotmatics.com>
2023-04-17 09:55:31 +0200

    feat: add second task with interface implementation

---
+++ slides/05-immutability-interfaces-and-proj-struct/interface-implementation-task.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <map>
+
+class Currency {
+ public:
+  Currency() = default;
+  virtual ~Currency() = default;
+
+  virtual double ConvertedToDollars(const Cantor& cantor) const = 0;
+  virtual std::string Abbreviation() const = 0;
+  virtual double Amount() const = 0;
+};
+
+class Cantor {
+ public:
+  Cantor() = default;
+  virtual ~Cantor() = default;
+
+  virtual double Rate(const std::string& abbreviation) const = 0;
+};
+
+class FakeUsdCantor : public Cantor {
+ public:
+  FakeUsdCantor() {
+    // rates_ map with values for ten most popular currencies relative to USD
+    rates_ = {
+      {"EUR", 0.85},
+      {"JPY", 110.66},
+      {"GBP", 0.73},
+      {"CHF", 0.91},
+      {"CAD", 1.25},
+      {"AUD", 1.34},
+      {"CNY", 6.47},
+      {"HKD", 7.78},
+      {"NZD", 1.44},
+      {"KRW", 1153.89}
+    };
+  }
+
+  double Rate(const std::string& abbreviation) const override {
+    auto it = rates_.find(abbreviation);
+    if (it != rates_.end()) {
+      return it->second;
+    }
+    // Handle error case when currency abbreviation is not found
+    return 0.0;
+  }
+
+ private:
+  std::map<std::string, double> rates_;
+};
+
+int main() {
+  return 0;
+}
